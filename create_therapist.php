cdbaa3ac868c9af26bb22e73bdd98b0f3628bde2
Simplyvenom <lawrencemunenex@gmail.com>
2023-10-04 22:41:49 +0300

    first commit

---
+++ create_therapist.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <title>Create Therapist</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            text-align: center;
+            background-color: brown;
+            color: white;
+            padding: 10px;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: white;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            font-weight: bold;
+        }
+
+        input[type="text"],
+        input[type="email"],
+        select {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        select {
+            appearance: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            padding: 8px;
+        }
+
+        button[type="submit"] {
+            background-color: brown;
+            color: white;
+            border: none;
+            border-radius: 3px;
+            padding: 10px 20px;
+            cursor: pointer;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Create Therapist</h1>
+    <form action="connect/post_therapist.php" method="POST">
+        <label for="first_name">First Name</label>
+        <input type="text" name="first_name" required><br>
+        <label for="last_name">Last Name:</label>
+        <input type="text" name="last_name" required><br>
+        <label for="speciality">Speciality</label>
+        <input type="text"  name="speciality" required><br>
+        <label for="gender">Gender:</label>
+        <select  name="gender" required>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+        </select><br>
+
+        <label for="city">City</label>
+        <select name="city" required>
+            <option value="Nairobi">Nairobi</option>
+            <option value="Nakuru">Nakuru</option>
+            <option value="Kisumu">Kisumu</option>
+            <option value="Mombasa">Mombasa</option>
+            <option value="Eldoret">Eldoret</option>
+        </select><br>
+
+        <label for="email">Email</label>
+        <input type="text"  name="email" required><br>
+
+        <button type="submit">Create Therapist</button>
+    </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #ddd;
+        }
+        </style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>User id</th>
+            <th>First_name</th>
+            <th>Last_name</th>
+            <th>Speciality</th>
+            <th>Gender</th>
+            <th>City</th>
+            <th>Email</th>
+        </tr>
+
+        <?php
+
+$dbServername = "localhost";
+$dbUsername = "lawrence";
+$dbPassword = "Kikoto12.";
+$dbName = "therapist";
+
+// Create a database connection
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// Check if the connection was successful
+if (!$conn) {
+    die('Could not connect: ' . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM therapist";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['first_name'] . "</td>";
+    echo "<td>" . $row['last_name'] . "</td>";
+    echo "<td>" . $row['speciality'] . "</td>";
+    echo "<td>" . $row['gender'] . "</td>";
+    echo "<td>" . $row['city'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "</tr>";
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
+
+    </table>
+
+</body>
+
+</html>
